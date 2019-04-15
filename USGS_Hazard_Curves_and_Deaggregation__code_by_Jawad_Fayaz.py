ab7aaee83a9bbd855dc7be3cbf96367689ce093e
jfayaz <jfayaz@uci.edu>
2019-04-14 20:08:49 -0700

    updated code

--- USGS_Hazard_Curves_and_Deaggregation__code_by_Jawad_Fayaz.py
+++ USGS_Hazard_Curves_and_Deaggregation__code_by_Jawad_Fayaz.py
@@ -24,20 +24,23 @@ INPUT:
 The input data must be provided in form of Excel file as per the given 'Input Data.xlsx' file 
 The name of the excel file must be kept as 'Input Data.xlsx'
 Row 1 of the file must contain the titles as follows:
-   Edition	Region	Longitude	Latitude	imt	vs30	Return Period
+   Edition	Region	Longitude	Latitude    Period	vs30	Return Period
 
 The input data must be provided starting from row 2 of the sheet with the required 
 values under each title. More than 1 rows can be provided as the data 
-E.g. the example file 'Input Data.xlsx' contains input for 3 sites 
+E.g. the example file 'Input Data.xlsx' contains input for 2 sites 
 
 Following are the options that can be provided under each title:   
-    Edition (USGS edition)      :   'E2008' , 'E2014'
+    Edition (USGS edition)      :   '2008' , '2014'
     Region                      :   'COUS', 'WUS', 'CEUS'  ; {WUS: Western US, CEUS: Central Eastern US}
     Longitude                   :    Longitude of the Site
     Latitude                    :    Latitude of the Site
-    imt (Intensity Measure Type):    'PGA', 'SA0P2', 'SA1P0','SA2P0' ; {PGA: Peak Ground Acceleration, SA0P2: Spectral Acceleration at 0.2 secs, SA1P0: Spectral Acceleration at 1 sec, SA2P0: Spectral Acceleration at 2 sec}
-    vs30 (Shear-Wave Velocity)  :    '180', '259', '360', '537', '760', '1150', '2000' ; {in m/s , restricted to only these values}
-
+    Period                      :    First Mode Period of the Structure (sec) (Downloads PGA if Period = 0)
+               Note: Closest results to the available USGS periods will be provided. USGS has results only for PGA, SA @ 0.2 sec, SA @ 1.0 sec, SA @ 2 sec     
+    vs30 (Shear-Wave Velocity)  :    Shear wave velocity at 30 meters at the site (m/s)
+               Note: Closest results to the available USGS vs30s will be provided. USGS has results only for '180', '259', '360', '537', '760', '1150' ; {in m/s , restricted to only these values}
+    Return Period (Hazard)      :    Return Period Hazard (in years)
+               Note: Closest results to the available USGS Hazard Levels will be provided. USGS has results only for '475', '975', '2475'
 
 
 OUTPUT:
@@ -52,7 +55,7 @@ The file will contain 2 sheets:
         Each table will have titles:
             source	r	m	ε	longitude	latitude	azimuth	  % contribution
             
-Note: If a USGS branch other than 'Gutenberg-Richter (gr)' and 'Characteristic (ch)' is used in deaggregation, the results wont be provided for now!
+Note: If a USGS branch other than 'afault' and 'bfault' is used in deaggregation, the results wont be provided for now!
 You are welcome to make the additions to the code to make it more exhaustive
 
 
@@ -73,14 +76,31 @@ import openpyxl
 
 
 # Reading given data
-df = pd.read_excel('Input Data.xlsx', converters={'Edition':str,'Region':str,'imt':str})
-sfmt = '{Edition}/{Region}/{Longitude}/{Latitude}/{imt}/{vs30}/{Return Period}'.format
-imt_list = ['PGA', 'SA0P2', 'SA1P0','SA2P0']
+data             = pd.read_excel('Input Data.xlsx', converters={'Edition':str,'Region':str,'imt':str})
+sfmt             = '{Edition}/{Region}/{Longitude}/{Latitude}/{imt}/{vs30}/{Return Period}'.format
+imt_list         = ['PGA', 'SA0P2', 'SA1P0','SA2P0']
+USGS_Sa_T        = pd.DataFrame(columns=['T','imt'])
+USGS_Sa_T['T']   = np.array([0,0.2,1,2])
+USGS_Sa_T['imt'] = imt_list
+USGS_RP          = np.array([475,975,2475])
+USGS_Vs30        = np.array([180,259,360,537,760,1150])
+df               = pd.DataFrame(columns=['Edition','Region','Longitude','Latitude','imt','vs30','Return Period'])
+df['Edition']    = data['Edition'].apply(lambda x: 'E'+x)
+df['Longitude']  = data['Longitude']
+df['Latitude']   = data['Latitude']
+diff_periods     = data['Period'].apply(lambda x: abs(x-np.array(USGS_Sa_T['T']))) 
+diff_vs30        = data['vs30'].apply(lambda x: abs(x-USGS_Vs30)) 
+diff_hazards     = data['Return Period'].apply(lambda x: abs(x-USGS_RP)) 
+df['Return Period'] = USGS_RP[diff_hazards.apply(lambda x: np.argmin(x))]
+df['vs30']       = USGS_Vs30[diff_vs30.apply(lambda x: np.argmin(x))]
+df['Region']     = data['Region']
+for i in diff_periods.apply(lambda x: np.argmin(x)): df['imt'] = USGS_Sa_T['imt'][i]
 
 
 
 for ii in range(0,len(df)):
     
+    print('\nDownloading Hazard Curves for Site {}...\n'.format(np.round(ii+1,0)))
     ### ---------- HAZARD CURVES ---------- ###
         
     def url_resp_values(row):
@@ -98,10 +118,8 @@ for ii in range(0,len(df)):
         lm['imt'] = imt_list[j] 
         params=lm.apply(lambda x: sfmt(**x), 1)
         response1, response2 = url_resp_values(params[0])
-        
         if j == 0:
-            DF_HAZARD_CURVES = pd.DataFrame.from_dict(response1)
-            
+            DF_HAZARD_CURVES = pd.DataFrame.from_dict(response1)            
         DF_HAZARD_CURVES = pd.concat([DF_HAZARD_CURVES, pd.DataFrame.from_dict(response2)], axis=1)  
         
     DF_HAZARD_CURVES.columns = ['Acceleration (g)','lambda PGA', 'lambda Sa at 0.2 sec', 'lambda Sa at 1 sec','lambda Sa at 2 sec']
@@ -115,8 +133,10 @@ for ii in range(0,len(df)):
     worksheet.write('A3', 'Vs30 (m/s)')
     worksheet.write('B3', lm['vs30'][0])
     
+
+
     
-    
+    print('Downloading Deaggregation Results for Site {}...\n\n'.format(np.round(ii+1,0)))    
     ### ---------- DEAGGREGATION ---------- ###
     
     lm=df[ii:ii+1].reset_index(drop=True)
@@ -133,30 +153,24 @@ for ii in range(0,len(df)):
         response.close()
               
     # Extracting sources from response
-    data = json.loads(response_1)
-       
+    data = json.loads(response_1)  
     # json data starts with response->data->soruces
-    lx = pd.DataFrame.from_dict(data['response'][0]['data'][0]['sources'])
-    
+    lx = pd.DataFrame.from_dict(data['response'][0]['data'][0]['sources'])    
     # Removing if contains pointsourcefinite
     lx=lx[~lx['name'].str.contains("PointSourceFinite")]
-    epsilon = lx.columns[10]
-    
+    epsilon = lx.columns[10]    
     # Rearrange columns
-    lx=lx[['name','source','r','m',epsilon,'longitude','latitude','azimuth','contribution']]
-    
+    lx=lx[['name','source','r','m',epsilon,'longitude','latitude','azimuth','contribution']]    
     # Deleting source column
     del lx['source']
-    
     # Renaming column to source
-    lx=lx.rename(columns={'name':'source'})
-    
+    lx=lx.rename(columns={'name':'source'})    
     #Getting indexes of faults
     Fault_Name_idx = np.asarray(np.where(lx['r'].isna()))
     Fault_Types = lx['source'][Fault_Name_idx[0]]
     Fault_Name_idx = np.append(Fault_Name_idx,[[len(lx)]],axis=1)
     position1 = 0
-    position2 = 2
+    position2 = 2 
     
     #bFault type
     if(Fault_Types[Fault_Types.str.contains("bFault")].any()):
@@ -181,8 +195,7 @@ for ii in range(0,len(df)):
                 position2 = position1 + len(bFault_list[i]) + 3
         del bFault_list
         del bFault_idx_true
-                
-                
+                                
     #aFault type
     if(Fault_Types[Fault_Types.str.contains("aFault")].any()):
         aFault_idx =  Fault_Types[Fault_Types.str.contains("aFault")==True].to_frame().reset_index()
